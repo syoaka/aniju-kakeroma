5b868fcd39a0abf0e381d7ee17cfc7194bda6846
Syota Akasaka <akaakaaka.com@gmail.com>
2022-11-22 21:36:12 +0900

    user.postテーブル作成

--- db/migrate/20221122122851_create_posts.rb
+++ db/migrate/20221122122851_create_posts.rb
@@ -2,6 +2,12 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
 
+      t.string     :title,   null: false
+      t.text       :text,    null: false
+      t.integer    :area_id, null: false
+      t.text       :image
+      t.references :user,    null: false, foreign_key: true
+
       t.timestamps
     end
   end
