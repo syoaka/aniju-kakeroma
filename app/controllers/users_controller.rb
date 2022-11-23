cfc0545b05e1b13636b539f7817af039df02433b
Syota Akasaka <akaakaaka.com@gmail.com>
2022-11-24 05:40:27 +0900

    ユーザーページ実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  def show
+    @name = current_user.name
+    @posts = current_user.posts
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 
   resources :posts
+  resources :users, only: :show
 
 end
