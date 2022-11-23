1f8dab9ff0ca621e8872ea680ee8540e88e8ca1f
Syota Akasaka <akaakaaka.com@gmail.com>
2022-11-23 15:37:38 +0900

    新規登録画面作成

--- db/migrate/20221122121224_devise_create_users.rb
+++ db/migrate/20221122121224_devise_create_users.rb
@@ -7,8 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string  :name,               null: false
-      t.text    :text,               null: false
-      t.text    :image
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 2022_11_22_122851) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.text "text", null: false
-    t.text "image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
